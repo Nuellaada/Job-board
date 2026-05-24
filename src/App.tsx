56b1125fab692f968d33c4fc89ec35b7460e1c92
Nuellaada <adaezesimon.sa@gmail.com>
2026-05-25 00:36:41 +0100

    error fixed 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import JobList from "./JobList";
+import JobList from './JobList';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <div className="app-header">
-      <h1>Find the Perfect Job for You</h1>
-      <p>Browse through our curated list of top engineering roles</p>
+        <h1>Find the perfect job for you</h1>
+        <p>Browse through our curated list of top engineering roles across Nigeria</p>
       </div>
       <JobList />
     </div>
